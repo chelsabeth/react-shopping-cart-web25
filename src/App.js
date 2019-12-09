57f21a9ba02c7c2cdee0ca589e2305ebd4f25c72
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-09 16:59:03 -0500

    refactor Products

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
