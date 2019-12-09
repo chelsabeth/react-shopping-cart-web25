1eead5aa3b941eb8f2d3ee4b7da2e3478a0d1f2e
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-09 16:42:44 -0500

    refactor routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from "./contexts/ProductContext";
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -17,6 +19,7 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -24,19 +27,15 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				components={Products}
 			/>
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				components={ShoppingCart}
 			/>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
